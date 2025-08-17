b7000d1b102db963ffb4543e7cff325c5348bf80
Dmitry Volkov <99538385+Avdors@users.noreply.github.com>
2025-08-17 06:56:15 +0100

    add magel popular

---
+++ lib/models/popular_model.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class PopularDietModel {
+  String? name;
+  String? iconPath;
+  String? level;
+  String? duration;
+  String? calories;
+  bool? viewIsSelected;
+  Color? boxColor;
+
+  PopularDietModel({
+    required this.name,
+    required this.iconPath,
+    required this.level,
+    required this.duration,
+    required this.calories,
+    required this.viewIsSelected,
+    this.boxColor = const Color(0xff92A3FD), // default color if not provided
+  });
+
+  static List<PopularDietModel> getPopularDiets() {
+    List<PopularDietModel> popularDiets = [];
+
+    popularDiets.add(PopularDietModel(
+      name: "Bluebarry Pancakes",
+      iconPath: "assets/icon/popular.svg",
+      level: "Medium",
+      duration: "30 mins",
+      calories: "230 kcal",
+      viewIsSelected: true,
+    ));
+
+    popularDiets.add(PopularDietModel(
+      name: "Salmon nigiri",
+      iconPath: "assets/icon/popular.svg",
+      level: "Easy",
+      duration: "10 mins",
+      calories: "200 kcal",
+      viewIsSelected: false,
+    ));
+
+    return popularDiets;
+  }
+}
